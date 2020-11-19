3951b8c90883bf9b894f069660dd22fade6b1413
Béatrice Capolla <beatrice.capolla@outlook.com>
2020-11-19 14:04:25 -0500

    R script update

--- Intro_to_bayesian/intro_to_bayesian.R
+++ Intro_to_bayesian/intro_to_bayesian.R
@@ -27,14 +27,9 @@ Sys.setenv(PATH = paste("C:/Rtools/mingw_64/bin", Sys.getenv("PATH"), sep=";"))
 install.packages("rstan")
 library(rstan)
 
-# Load Stan file ####
-fit = stan("workshops/Intro_to_bayesian/bayesian_model.stan", model_name = "bayesian_model", data = list())
-stan_code <- readChar(bayesian_model, file.info(bayesian_model)$size)
-cat(stan_code)
-
 # Run Stan ####
-fit = stan(model_code = bayesian_model.stan, data = list(tarsus = BTdata$tarsus, n_obs = 828), iter = 5000)
-
 data_for_stan_model <- list(n_obs = nrow(BTdata), n_dam = nrow(dam_ID), tarsus = BTdata$tarsus, sex_no = BTdata$sex_no, dam_no = BTdata$dam_no)
-tarsus_model <- stan_model("Intro_to_bayesian/bayesian_model.stan")
-result <- sampling(tarsus_model, data = data_for_stan_model)
+
+results <- stan(file = "Intro_to_bayesian/bayesian_model.stan", data = data_for_stan_model, iter = 500, chains = 5, warmup = 250)
+
+
