d2ef784837107212ed0acf84b3a93e99883e98cb
Béatrice Capolla <beatrice.capolla@outlook.com>
2021-03-09 14:32:35 -0500

    Version finale

--- modelling_count_data/script_modelling_count_data.R
+++ modelling_count_data/script_modelling_count_data.R
@@ -13,11 +13,11 @@ data <- mutate(data, Site = as.factor(Site),
 data <- data %>%
   mutate(Height_class = cut(Height_cm,
                         breaks = c(1, 10, 30, Inf),
-                        labels = c("1-10", "11-30", ">30")))  # Add a column to your dataframe where the height class
+                        labels = c("1-10", "11-30", ">30")))  # Adds a column to your dataframe where the height class
                                                               # is specified (1-10 cm, 16-30 cm, or more than 30 cm).
 data_summary <- data %>%
   group_by(Site, Plot, Height_class) %>%
-  summarize(n_line = n())  # Summurizes your dataframe by site, plot and height class.
+  summarize(n_line = n())  # Summarizes your dataframe by site, plot and height class.
 
 ##Create empty dataframe
 empty_df <- expand.grid(
@@ -34,10 +34,10 @@ reorganized_data <- right_join(data_summary, empty_df,
                                                                         # with no match in empty_df will have NA values in the new columns.
 
 names(reorganized_data)[names(reorganized_data) == "n_line"] <- "freq"  # Changes the name of the column
-                                                                         # n_line to freq.
+                                                                        # "n_line" to "freq".
 
 
-reorganized_data$freq[is.na(reorganized_data$freq)] = 0  # CHanges all NAs value in the freq column to 0.
+reorganized_data$freq[is.na(reorganized_data$freq)] = 0  # Changes all NAs in the freq column to 0.
 
 
 #Choosing the right distribution
@@ -49,7 +49,7 @@ library(ggplot2)
 ##Data
 data(Owls)  # This dataframe is included in the glmmTMB package.
 str(Owls)  # The dependent variable, SiblingNegotiation, represents the number of calls emitted by chicks in a nest awaiting food, 
-           # depending on their level of hunger (FoodTreatment), the sex of the parent foraging, (SexParent), and the arrival time 
+           # depending on their level of hunger (FoodTreatment), the sex of the parent foraging (SexParent), and the arrival time 
            # of that parent (ArrivalTime). Since repeated measurements have been taken on each nest, Nest is a random effect. 
            # Finally, since the number of chicks (BroodSize) varies in each nest, this variable will be used as offset in order to 
            # model the number of calls per chick.
@@ -68,12 +68,12 @@ chi2 <- sum(residuals(owls_p, type = "pearson")^2)  # Calculating Chi2 value to 
 chi2 / df.residual(owls_p)  # Calculating the dispersion coefficient
 1 - pchisq(chi2, df = df.residual(owls_p))  # Calculating the p-value
 
-  # Note here that the Chi2 test is unilateral, so it only tests for overdispersion. However, underdispersion is less common and generaly
+  # Note here that the Chi2 test is unilateral, so it only tests for overdispersion. However, underdispersion is less common and generally
   # has less influence on the model estimates. A "normal" dispersion coefficient is around 1 : a lower coefficient would be a sign of
   # underdispersion, while a greater one would indicate overdispersion. A p-value lower than 0.05 means that your data are significantly
   # overdispersed. A p-value really close to 1 can indicate over-parameterization.
   # In the present case, the dispersion coefficient is approx. 5.5, and the p-value is almost 0. This indicates that we have overdispersion.
-  # To adress this problem, we will rerun the model, but with a negative binomial distribution.
+  # To address this problem, we will rerun the model, but with a negative binomial distribution.
 
 ###Negative binomial distribution
 owls_nb <- glmmTMB(SiblingNegotiation ~ FoodTreatment * SexParent + ArrivalTime +
@@ -120,7 +120,7 @@ printCoefmat(quasi_table(owls_p), digits = 3)  # This applies the quasi_table fu
                                                # the Poisson distribution.
 
   # As you can see, the estimates are not very different when using the negative binomial or quasi-poisson distribution. However,
-  # as the quasi_table function only produces a table of the ajusted estimates, it may be easier to work with the glmmTMB function
+  # as the quasi_table function only produces a table of the adjusted estimates, it may be easier to work with the glmmTMB function
   # using the negative binomial distribution, which creates an object.
   # If the data were underdispersed, you can use both quasi-Poisson and generalized Poisson distributions.
 
@@ -149,8 +149,8 @@ ggplot(owls_pred, aes(x = 1:nrow(owls_pred), y = pred, ymin = q025, ymax = q975)
   geom_line() +
   geom_point(aes(y = SiblingNegotiation))  # Produces a graph with the intervals in gray, a line representing the simulated mean
                                            # and the real data as black dots. As you can see, even if the CI contains the large
-                                           # majority of the dots, it seems too narrow in the left part of the graph and too large
-                                           # in the right part.
+                                           # majority of the dots, it seems too narrow on the left of the graph and too large
+                                           # on the right.
 
 ## Numerical method to test if there is an excess of zero
 nb_zeros <- apply(sim_owls_nb, 2, function(x) sum(x == 0))  # Calculates how many zeros are simulated with the nb model.
@@ -159,14 +159,14 @@ c(quantile(nb_zeros, probs = 0.025), quantile(nb_zeros, probs = 0.975))  # CI 2.
 sum(Owls$SiblingNegotiation == 0)  # The real number of zeros in your data.
 
   # As you can see, the model simulates between approx. 85 and 130 zeros, but your data actually contains 156 zeros. This
-  # indicates that the model doesn't simulate enough zeros. To have a model ajusted to your data, you need to consider this
+  # indicates that the model doesn't simulate enough zeros. To have a model adjusted to your data, you need to consider this
   # excess of zeros in your model.
 
 ##Model considering the excess of zeros
 owls_zinb <- glmmTMB(SiblingNegotiation ~ FoodTreatment * SexParent + ArrivalTime +
                        (1|Nest) + offset(logBroodSize),
                      family = nbinom2, ziformula = ~1, data = Owls)  # Adding "ziformula = ~1" creates a model in two parts.
-                                                                     # The first part has with a p0 probability, where we have
+                                                                     # The first part has a p0 probability, where we have
                                                                      # a structural zero (y = 0). With the remaining 1-p0 probability,
                                                                      # y follows a Poisson or negative binomial distribution which can,
                                                                      # of course, also produce occasionally some zeros.
@@ -195,5 +195,5 @@ ggplot(owls_pred, aes(x = 1:nrow(owls_pred), y = pred, ymin = q025, ymax = q975)
   geom_point(aes(y = SiblingNegotiation), alpha = 0.5)  # Produces a graph with the nb CI and mean in green, and the zinb CI
                                                         # (brownish when over the nb CI) and mean in orange. The zinb model  
                                                         # seems more adjusted to the real data (black dots), with a larger CI
-                                                        # in the right part of the graph, and a narrower CI in the left part.
+                                                        # on the right of the graph, and a narrower CI on the left.
 
